67ea5c2e435ababb93cd80513846658cff027b31
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2021-05-05 00:21:56 +0900

    About-page editing

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,11 +14,34 @@ const About = ({ data, location }) => {
     // pageImgWidth: childImageSharp.original.width,
     // pageImgHeight: childImageSharp.original.height,
   }
+
   return (
     <Layout className="about">
       <Seo postMeta={metadata} />
       <article className="container">
+
         <h1>About me</h1>
+
+        <h2>Tools</h2>
+        <p>私が使用しているツールです。</p>
+
+        <h3>Software</h3>
+        <ul>
+          <li>Coding: Visual Studio Code with New Moon Theme</li>
+          <li>Terminal: iTerm2</li>
+          <li>Design: Affinity series
+            <ul>
+              <li>Publisher</li>
+              <li>Designer</li>
+              <li>Photo</li>
+            </ul>
+          </li>
+        </ul>
+
+        <h3>Hardware</h3>
+        <ul>
+          <li>Coding PC: MacBook Pro (15-inch, 2019)</li>
+        </ul>
 
       </article>
     </Layout>
